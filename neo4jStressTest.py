12a72381eb8ae8b05b37e8dad0c50dc25cb9ad1f
cosimosimeoneTR <cosimo.simeone@thomsonreuters.com>
2015-10-14 16:14:33 +0200

    First version of stress test python

---
+++ neo4jStressTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import json
+from py2neo import Graph, authenticate, Node
+
+authenticate("localhost:7474", "neo4j", "neo4j123")
+graph = Graph()
+
+
+results = graph.cypher.execute("MATCH (Company{companyName:'Cmp_1'})-[x:PRODUCES]->(Drug)<-[r:RELATED_TO]-(Trial)-[z:RELATED_TO]->(anotherDrug) RETURN Company,Drug,Trial,anotherDrug LIMIT 60")
+
