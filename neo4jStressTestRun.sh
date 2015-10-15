0f3d89de0264395ac37093fb0e2594b83ef12458
cosimosimeoneTR <cosimo.simeone@thomsonreuters.com>
2015-10-15 11:47:42 +0200

    platform check -fixed

--- neo4jStressTestRun.sh
+++ neo4jStressTestRun.sh
@@ -11,19 +11,24 @@ export neoUrl=$2
 
 #export LOGFILE="log/neo4jStressTestRunLog_$1.`date +%Y%m%d%H%M`.log"
 export LOGFILE="log/neo4jStressTestRunLog_$1.log"
+export arch=`uname -a | cut -d " " -f 1`
+pids=""
 
 export instanceType=`wget -q -O - http://instance-data/latest/meta-data/instance-type || echo REMOTE`
 #echo $instanceType
 
-pids=""
 
-for i in `seq 1 3`; do
+for i in `seq 1 2`; do
    let loopNums=10**$i
    for i in `seq 1 $loopNums`; do
       ./neo4jStressTest.py $neoUrl $loopNums $instanceType >> $LOGFILE &
-      pids="$pids $!"
-   done    
+      #pids="$pids $!"
+      # Don't stress poor Window$...
+      if [ "$arch" != "Linux" ]
+      then
+        sleep 1
+      fi
+   done
 
-   wait $pids
+   #wait $pids
 done
-
