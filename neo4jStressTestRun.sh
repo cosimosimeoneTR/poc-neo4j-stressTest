90bd62b7f81ce1968a1aca335de7aa46c7d4bce1
cosimosimeoneTR <cosimo.simeone@thomsonreuters.com>
2015-10-15 12:05:19 +0200

    some more help to window$

--- neo4jStressTestRun.sh
+++ neo4jStressTestRun.sh
@@ -12,24 +12,29 @@ export neoUrl=$2
 #export LOGFILE="log/neo4jStressTestRunLog_$1.`date +%Y%m%d%H%M`.log"
 export LOGFILE="log/neo4jStressTestRunLog_$1.log"
 export arch=`uname -a | cut -d " " -f 1`
-pids=""
+export pids=""
+export procs=0
+
 
 export instanceType=`wget -q -O - http://instance-data/latest/meta-data/instance-type || echo REMOTE`
 #echo $instanceType
 
 
-for i in `seq 1 2`; do
+for i in `seq 1 3`; do
    let loopNums=10**$i
    for i in `seq 1 $loopNums`; do
       ./neo4jStressTest.py $neoUrl $loopNums $instanceType >> $LOGFILE &
-      #pids="$pids $!"
+      let procs=procs+1
+      pids="$pids $!"
       # Don't stress poor Window$...
-      if [ "$arch" != "Linux" ]
-      then
-        sleep 1
+      if [ "$arch" != "Linux" ] && [ "$procs" -gt "399" ]; then
+        sleep 3
+        procs=0
+        #echo "sleeping..."
       fi
    done
 
-   #wait $pids
+   wait $pids
+   pids=""
 done
 
