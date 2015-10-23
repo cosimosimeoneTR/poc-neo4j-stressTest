e188370d3375c77100619fc7ec4696d6ee01c16d
Debian <admin@ip-172-31-2-163.ec2.internal>
2015-10-23 08:45:36 +0000

    Added another query, that's basically a "PING" just to see if system is still alive

--- neo4jStressTest.py
+++ neo4jStressTest.py
@@ -13,6 +13,7 @@ query[2] = "MATCH (Company{attr1:'asqwdasdasda'})-[x:CMP2DRG]->(Drug)<-[r:TRL2DR
 query[3] = "MATCH (Company{attr9:'asqwdasdasda'})-[x:CMP2DRG]->(Drug)<-[r:TRL2DRG1]-(Trial)-[z:TRL2DRG2]->(anotherDrug) RETURN Company,Drug,Trial,anotherDrug "
 query[4] = "MATCH (Drug{attr1:'asqwdasdasda'})<-[r:TRL2DRG2]-(Trial)-[z:TRL2DRG3]->(anotherDrug) RETURN Drug,Trial,anotherDrug "
 query[5] = "MATCH (Drug{attr9:'asqwdasdasda'})<-[r:TRL2DRG3]-(Trial)-[z:TRL2DRG1]->(anotherDrug) RETURN Drug,Trial,anotherDrug "
+query[6] = "MATCH (n:Company) RETURN n LIMIT 2"
 rndQuery=random.randint(0, len(query)-1)
 rndQueryVal=random.randint(0, 1000)
 queryToRun = query[rndQuery].replace('#',str(rndQueryVal))
