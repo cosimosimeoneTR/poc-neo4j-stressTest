c597d8afdd8d6942aa122f1e87706c756049cd6e
Debian <admin@ip-172-31-63-213.ec2.internal>
2015-10-29 08:12:06 +0000

    More indexes shells

---
+++ createIndexes.sh
@@ -0,0 +1,17 @@
+neo4j-shell
+
+
+create index on :trial(attr5)           ;
+create index on :Disease(diseaseId)     ;
+create index on :Disease(id)            ;
+create index on :Disease(attr1)         ;
+create index on :Drugs(attr1)           ;
+create index on :Drugs(attr0)           ;
+create index on :Drugs(drugId)          ;
+create index on :Disease(diseaseName)   ;
+create index on :trial(trialId)         ;
+create index on :trial(trialName)       ;
+create index on :Company(companyName)   ;
+
+schema ls
+
