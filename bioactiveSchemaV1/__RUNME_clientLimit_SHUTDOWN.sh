bcbc43c6a29534a7f4acf0ef38a798fe03ca00f5
Debian <admin@ip-172-31-33-38.eu-west-1.compute.internal>
2015-11-25 07:33:53 +0000

    Changes

--- bioactiveSchemaV1/__RUNME_clientLimit_SHUTDOWN.sh
+++ bioactiveSchemaV1/__RUNME_clientLimit_SHUTDOWN.sh
@@ -1,28 +1,14 @@
-#./_RUNME_clientLimit.sh 10_1 10 2;sleep 60;./_RUNME_clientLimit.sh 10_2 10 2;sleep 60
-#./_RUNME_clientLimit.sh 30_1 30 2;sleep 60;./_RUNME_clientLimit.sh 30_2 30 2;sleep 60
-#./_RUNME_clientLimit.sh 50_1 50 2;sleep 60;./_RUNME_clientLimit.sh 50_2 50 2;sleep 60
-#./_RUNME_clientLimit.sh 100_1 100 2;sleep 60;./_RUNME_clientLimit.sh 100_2 100 2;sleep 60
-#./_RUNME_clientLimit.sh 130_1 130 2;sleep 60;./_RUNME_clientLimit.sh 130_2 130 2;sleep 60
-#./_RUNME_clientLimit.sh 150_1 150 2;sleep 60;./_RUNME_clientLimit.sh 150_2 150 2;sleep 60
-#./_RUNME_clientLimit.sh 10_1 10 2;sleep 60;./_RUNME_clientLimit.sh 10_2 10 2;sleep 60
-#./_RUNME_clientLimit.sh 30_1 30 2;sleep 60;./_RUNME_clientLimit.sh 30_2 30 2;sleep 60
-#./_RUNME_clientLimit.sh 50_1 50 2;sleep 60;./_RUNME_clientLimit.sh 50_2 50 2;sleep 60
-#./_RUNME_clientLimit.sh 100_1 100 2;sleep 60;./_RUNME_clientLimit.sh 100_2 100 2;sleep 60
-#./_RUNME_clientLimit.sh 130_1 130 2;sleep 60;./_RUNME_clientLimit.sh 130_2 130 2;sleep 60
-#./_RUNME_clientLimit.sh 150_1 150 2;sleep 60;./_RUNME_clientLimit.sh 150_2 150 2;sleep 60
-
-
 ./_RUNME_clientLimit.sh 10_3 10 2;sleep 60;./_RUNME_clientLimit.sh 10_4 10 2;sleep 60
 ./_RUNME_clientLimit.sh 30_3 30 2;sleep 60;./_RUNME_clientLimit.sh 30_4 30 2;sleep 60
 ./_RUNME_clientLimit.sh 50_3 50 2;sleep 60;./_RUNME_clientLimit.sh 50_4 50 2;sleep 60
 ./_RUNME_clientLimit.sh 100_3 100 2;sleep 60;./_RUNME_clientLimit.sh 100_4 100 2;sleep 60
 ./_RUNME_clientLimit.sh 130_3 130 2;sleep 60;./_RUNME_clientLimit.sh 130_4 130 2;sleep 60
 ./_RUNME_clientLimit.sh 150_3 150 2;sleep 60;./_RUNME_clientLimit.sh 150_4 150 2;sleep 60
-./_RUNME_clientLimit.sh 10_3 10 2;sleep 60;./_RUNME_clientLimit.sh 10_4 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_3 30 2;sleep 60;./_RUNME_clientLimit.sh 30_4 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_3 50 2;sleep 60;./_RUNME_clientLimit.sh 50_4 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_3 100 2;sleep 60;./_RUNME_clientLimit.sh 100_4 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_3 130 2;sleep 60;./_RUNME_clientLimit.sh 130_4 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_3 150 2;sleep 60;./_RUNME_clientLimit.sh 150_4 150 2;sleep 60
+./_RUNME_clientLimit.sh 10_5 10 2;sleep 60;./_RUNME_clientLimit.sh 10_6 10 2;sleep 60
+./_RUNME_clientLimit.sh 30_5 30 2;sleep 60;./_RUNME_clientLimit.sh 30_6 30 2;sleep 60
+./_RUNME_clientLimit.sh 50_5 50 2;sleep 60;./_RUNME_clientLimit.sh 50_6 50 2;sleep 60
+./_RUNME_clientLimit.sh 100_5 100 2;sleep 60;./_RUNME_clientLimit.sh 100_6 100 2;sleep 60
+./_RUNME_clientLimit.sh 130_5 130 2;sleep 60;./_RUNME_clientLimit.sh 130_6 130 2;sleep 60
+./_RUNME_clientLimit.sh 150_5 150 2;sleep 60;./_RUNME_clientLimit.sh 150_6 150 2;sleep 60
 
 sudo shutdown -h now
