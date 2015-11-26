47aa7ac01beba2b6893437da2899ad310de0bb46
Debian <admin@ip-172-31-40-206.eu-west-1.compute.internal>
2015-11-26 09:15:33 +0000

    Typo

--- bioactiveSchemaV1/__RUNME_clientLimit.sh
+++ bioactiveSchemaV1/__RUNME_clientLimit.sh
@@ -1,9 +1,9 @@
-./_RUNME_clientLimit.sh 10_7/g 10 2;sleep 60;./_RUNME_clientLimit.sh 10_8/g 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_7/g 30 2;sleep 60;./_RUNME_clientLimit.sh 30_8/g 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_7/g 50 2;sleep 60;./_RUNME_clientLimit.sh 50_8/g 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_7/g 100 2;sleep 60;./_RUNME_clientLimit.sh 100_8/g 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_7/g 130 2;sleep 60;./_RUNME_clientLimit.sh 130_8/g 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_7/g 150 2;sleep 60;./_RUNME_clientLimit.sh 150_8/g 150 2;sleep 60
+./_RUNME_clientLimit.sh 10_7 10 2;sleep 60;./_RUNME_clientLimit.sh 10_8 10 2;sleep 60
+./_RUNME_clientLimit.sh 30_7 30 2;sleep 60;./_RUNME_clientLimit.sh 30_8 30 2;sleep 60
+./_RUNME_clientLimit.sh 50_7 50 2;sleep 60;./_RUNME_clientLimit.sh 50_8 50 2;sleep 60
+./_RUNME_clientLimit.sh 100_7 100 2;sleep 60;./_RUNME_clientLimit.sh 100_8 100 2;sleep 60
+./_RUNME_clientLimit.sh 130_7 130 2;sleep 60;./_RUNME_clientLimit.sh 130_8 130 2;sleep 60
+./_RUNME_clientLimit.sh 150_7 150 2;sleep 60;./_RUNME_clientLimit.sh 150_8 150 2;sleep 60
 ./_RUNME_clientLimit.sh 10_9 10 2;sleep 60;./_RUNME_clientLimit.sh 10_A 10 2;sleep 60
 ./_RUNME_clientLimit.sh 30_9 30 2;sleep 60;./_RUNME_clientLimit.sh 30_A 30 2;sleep 60
 ./_RUNME_clientLimit.sh 50_9 50 2;sleep 60;./_RUNME_clientLimit.sh 50_A 50 2;sleep 60
