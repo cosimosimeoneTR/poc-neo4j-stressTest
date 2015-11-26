8a63548cdb481120e3499f05cadd7c3493da6dd7
Debian <admin@ip-172-31-40-206.eu-west-1.compute.internal>
2015-11-26 15:20:58 +0000

    New test, query 10

--- bioactiveSchemaV1/__RUNME_clientLimit.sh
+++ bioactiveSchemaV1/__RUNME_clientLimit.sh
@@ -1,14 +1,17 @@
-./_RUNME_clientLimit.sh 10_7 10 2;sleep 60;./_RUNME_clientLimit.sh 10_8 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_7 30 2;sleep 60;./_RUNME_clientLimit.sh 30_8 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_7 50 2;sleep 60;./_RUNME_clientLimit.sh 50_8 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_7 100 2;sleep 60;./_RUNME_clientLimit.sh 100_8 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_7 130 2;sleep 60;./_RUNME_clientLimit.sh 130_8 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_7 150 2;sleep 60;./_RUNME_clientLimit.sh 150_8 150 2;sleep 60
-./_RUNME_clientLimit.sh 10_9 10 2;sleep 60;./_RUNME_clientLimit.sh 10_A 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_9 30 2;sleep 60;./_RUNME_clientLimit.sh 30_A 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_9 50 2;sleep 60;./_RUNME_clientLimit.sh 50_A 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_9 100 2;sleep 60;./_RUNME_clientLimit.sh 100_A 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_9 130 2;sleep 60;./_RUNME_clientLimit.sh 130_A 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_9 150 2;sleep 60;./_RUNME_clientLimit.sh 150_A 150 2;sleep 60
+./_RUNME_clientLimit.sh query10_01cli_take1  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take2  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take1 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take2 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take1 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take2 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take1 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take2 50 10;sleep 60
+sleep 600
+./_RUNME_clientLimit.sh query10_01cli_take3  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take4  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take3 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take4 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take3 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take4 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take3 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take4 50 10;sleep 60
+sleep 600
+./_RUNME_clientLimit.sh query10_01cli_take5  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take5  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take5 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take5 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take5 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take5 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take5 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take5 50 10;sleep 60
 
-###############sudo shutdown -h now
+
+### sudo shutdown -h now
--- bioactiveSchemaV1/__RUNME_clientLimit_SHUTDOWN.sh
+++ bioactiveSchemaV1/__RUNME_clientLimit_SHUTDOWN.sh
@@ -1,14 +1,17 @@
-./_RUNME_clientLimit.sh 10_3 10 2;sleep 60;./_RUNME_clientLimit.sh 10_4 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_3 30 2;sleep 60;./_RUNME_clientLimit.sh 30_4 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_3 50 2;sleep 60;./_RUNME_clientLimit.sh 50_4 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_3 100 2;sleep 60;./_RUNME_clientLimit.sh 100_4 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_3 130 2;sleep 60;./_RUNME_clientLimit.sh 130_4 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_3 150 2;sleep 60;./_RUNME_clientLimit.sh 150_4 150 2;sleep 60
-./_RUNME_clientLimit.sh 10_5 10 2;sleep 60;./_RUNME_clientLimit.sh 10_6 10 2;sleep 60
-./_RUNME_clientLimit.sh 30_5 30 2;sleep 60;./_RUNME_clientLimit.sh 30_6 30 2;sleep 60
-./_RUNME_clientLimit.sh 50_5 50 2;sleep 60;./_RUNME_clientLimit.sh 50_6 50 2;sleep 60
-./_RUNME_clientLimit.sh 100_5 100 2;sleep 60;./_RUNME_clientLimit.sh 100_6 100 2;sleep 60
-./_RUNME_clientLimit.sh 130_5 130 2;sleep 60;./_RUNME_clientLimit.sh 130_6 130 2;sleep 60
-./_RUNME_clientLimit.sh 150_5 150 2;sleep 60;./_RUNME_clientLimit.sh 150_6 150 2;sleep 60
+./_RUNME_clientLimit.sh query10_01cli_take1  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take2  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take1 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take2 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take1 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take2 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take1 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take2 50 10;sleep 60
+sleep 600
+./_RUNME_clientLimit.sh query10_01cli_take3  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take4  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take3 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take4 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take3 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take4 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take3 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take4 50 10;sleep 60
+sleep 600
+./_RUNME_clientLimit.sh query10_01cli_take5  1 10;sleep 60;./_RUNME_clientLimit.sh query10_01cli_take5  1 10;sleep 60
+./_RUNME_clientLimit.sh query10_10cli_take5 10 10;sleep 60;./_RUNME_clientLimit.sh query10_10cli_take5 10 10;sleep 60
+./_RUNME_clientLimit.sh query10_30cli_take5 30 10;sleep 60;./_RUNME_clientLimit.sh query10_30cli_take5 30 10;sleep 60
+./_RUNME_clientLimit.sh query10_50cli_take5 50 10;sleep 60;./_RUNME_clientLimit.sh query10_50cli_take5 50 10;sleep 60
+
 
 sudo shutdown -h now
