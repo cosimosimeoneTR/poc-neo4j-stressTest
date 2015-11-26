0ffd516afac7d46c10a2c65ae0787f624d417c0b
Debian <admin@ip-172-31-40-206.eu-west-1.compute.internal>
2015-11-26 09:13:32 +0000

    __RUNME_clientLimit.sh without shutdown

---
+++ bioactiveSchemaV1/__RUNME_clientLimit.sh
@@ -0,0 +1,14 @@
+./_RUNME_clientLimit.sh 10_7/g 10 2;sleep 60;./_RUNME_clientLimit.sh 10_8/g 10 2;sleep 60
+./_RUNME_clientLimit.sh 30_7/g 30 2;sleep 60;./_RUNME_clientLimit.sh 30_8/g 30 2;sleep 60
+./_RUNME_clientLimit.sh 50_7/g 50 2;sleep 60;./_RUNME_clientLimit.sh 50_8/g 50 2;sleep 60
+./_RUNME_clientLimit.sh 100_7/g 100 2;sleep 60;./_RUNME_clientLimit.sh 100_8/g 100 2;sleep 60
+./_RUNME_clientLimit.sh 130_7/g 130 2;sleep 60;./_RUNME_clientLimit.sh 130_8/g 130 2;sleep 60
+./_RUNME_clientLimit.sh 150_7/g 150 2;sleep 60;./_RUNME_clientLimit.sh 150_8/g 150 2;sleep 60
+./_RUNME_clientLimit.sh 10_9 10 2;sleep 60;./_RUNME_clientLimit.sh 10_A 10 2;sleep 60
+./_RUNME_clientLimit.sh 30_9 30 2;sleep 60;./_RUNME_clientLimit.sh 30_A 30 2;sleep 60
+./_RUNME_clientLimit.sh 50_9 50 2;sleep 60;./_RUNME_clientLimit.sh 50_A 50 2;sleep 60
+./_RUNME_clientLimit.sh 100_9 100 2;sleep 60;./_RUNME_clientLimit.sh 100_A 100 2;sleep 60
+./_RUNME_clientLimit.sh 130_9 130 2;sleep 60;./_RUNME_clientLimit.sh 130_A 130 2;sleep 60
+./_RUNME_clientLimit.sh 150_9 150 2;sleep 60;./_RUNME_clientLimit.sh 150_A 150 2;sleep 60
+
+###############sudo shutdown -h now
