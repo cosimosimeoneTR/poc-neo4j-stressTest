077738757d1f4a432d18c4cf259b359acf4cf6d2
Debian <admin@ip-172-31-14-112.eu-west-1.compute.internal>
2015-11-30 14:34:46 +0000

    Another wrapper

---
+++ bioactiveSchemaV1/__RUNME_deleteUpdate.sh
@@ -0,0 +1,17 @@
+./_RUNME_deleteUpdate.sh query10_01cli_take1  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take2  1 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_10cli_take1 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take2 10 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_30cli_take1 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take2 30 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_50cli_take1 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take2 50 ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh query10_01cli_take3  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take4  1 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_10cli_take3 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take4 10 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_30cli_take3 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take4 30 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_50cli_take3 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take4 50 ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh query10_01cli_take5  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take6  1 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_10cli_take5 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take6 10 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_30cli_take5 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take6 30 ;sleep 6
+./_RUNME_deleteUpdate.sh query10_50cli_take5 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take6 50 ;sleep 6
+
+
+### sudo shutdown -h now
