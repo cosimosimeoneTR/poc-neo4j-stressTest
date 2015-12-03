48ad96b345855a214e38175a7834789e2cf505aa
Debian <admin@ip-172-31-41-42.eu-west-1.compute.internal>
2015-12-03 09:15:45 +0000

    All tests now

--- bioactiveSchemaV1/__RUNME_deleteUpdate.sh
+++ bioactiveSchemaV1/__RUNME_deleteUpdate.sh
@@ -1,17 +1,53 @@
-./_RUNME_deleteUpdate.sh query10_01cli_take1  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take2  1 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_10cli_take1 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take2 10 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_30cli_take1 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take2 30 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_50cli_take1 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take2 50 ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take1  1 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take2  1 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take1 10 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take2 10 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take1 30 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take2 30 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take1 50 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take2 50 U  ;sleep 6
 sleep 60
-./_RUNME_deleteUpdate.sh query10_01cli_take3  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take4  1 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_10cli_take3 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take4 10 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_30cli_take3 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take4 30 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_50cli_take3 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take4 50 ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take3  1 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take4  1 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take3 10 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take4 10 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take3 30 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take4 30 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take3 50 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take4 50 U  ;sleep 6
 sleep 60
-./_RUNME_deleteUpdate.sh query10_01cli_take5  1 ;sleep 6;./_RUNME_deleteUpdate.sh query10_01cli_take6  1 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_10cli_take5 10 ;sleep 6;./_RUNME_deleteUpdate.sh query10_10cli_take6 10 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_30cli_take5 30 ;sleep 6;./_RUNME_deleteUpdate.sh query10_30cli_take6 30 ;sleep 6
-./_RUNME_deleteUpdate.sh query10_50cli_take5 50 ;sleep 6;./_RUNME_deleteUpdate.sh query10_50cli_take6 50 ;#sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take5  1 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_01cli_take6  1 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take5 10 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_10cli_take6 10 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take5 30 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_30cli_take6 30 U  ;sleep 6
+./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take5 50 U  ;sleep 6;./_RUNME_deleteUpdate.sh updateNodeAddAttr_50cli_take6 50 U  ;#sleep 6
 
+
+sleep 60
+
+./_RUNME_deleteUpdate.sh deleteRels_01cli_take1  1 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_01cli_take2  1 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_10cli_take1 10 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_10cli_take2 10 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_30cli_take1 30 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_30cli_take2 30 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_50cli_take1 50 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_50cli_take2 50 DR ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh deleteRels_01cli_take3  1 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_01cli_take4  1 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_10cli_take3 10 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_10cli_take4 10 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_30cli_take3 30 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_30cli_take4 30 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_50cli_take3 50 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_50cli_take4 50 DR ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh deleteRels_01cli_take5  1 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_01cli_take6  1 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_10cli_take5 10 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_10cli_take6 10 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_30cli_take5 30 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_30cli_take6 30 DR ;sleep 6
+./_RUNME_deleteUpdate.sh deleteRels_50cli_take5 50 DR ;sleep 6;./_RUNME_deleteUpdate.sh deleteRels_50cli_take6 50 DR ;#sleep 6
+
+
+sleep 60
+
+
+./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take1  1 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take2  1 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take1 10 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take2 10 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take1 30 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take2 30 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take1 50 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take2 50 D  ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take3  1 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take4  1 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take3 10 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take4 10 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take3 30 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take4 30 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take3 50 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take4 50 D  ;sleep 6
+sleep 60
+./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take5  1 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_01cli_take6  1 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take5 10 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_10cli_take6 10 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take5 30 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_30cli_take6 30 D  ;sleep 6
+./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take5 50 D  ;sleep 6;./_RUNME_deleteUpdate.sh deleteNodeCascade_50cli_take6 50 D  ;sleep 6
 
 ### sudo shutdown -h now
